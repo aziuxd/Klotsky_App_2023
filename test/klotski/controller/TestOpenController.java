e1eb8fbcc9f018b5a16bad843726485222ccbc1e
aziuxd <118687529+aziuxd@users.noreply.github.com>
2023-06-28 17:28:13 +0200

    Add files via upload

---
+++ test/klotski/controller/TestOpenController.java
@@ -0,0 +1,24 @@
+package klotski.controller;
+
+import org.junit.Test;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.nio.file.Paths;
+import klotski.model.Board;
+import klotski.view.KlotskiApp;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestOpenController {
+    String pathFile = "C:/Users/decar/Desktop/Klotsky_App_2023-main/TestOpenCotroller";
+    Path path=  Paths.get(pathFile);
+    @Test 
+    public void TestOpen() {
+    Board b = new Board();
+	KlotskiApp app = new KlotskiApp(b);
+    OpenController Test =new OpenController(app, b, path);
+    assertTrue(Test.open());
+    }
+}
